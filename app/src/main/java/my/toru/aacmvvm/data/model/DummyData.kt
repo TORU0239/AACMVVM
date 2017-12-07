aa1815ae0da0a5b6e1cdd500691f7ec25911068a
Toru Choi <toru.choi@11street.my>
2017-12-07 17:34:11 +0800

    all delete

--- app/src/main/java/my/toru/aacmvvm/data/model/DummyData.kt
+++
@@ -1,6 +0,0 @@
-package my.toru.aacmvvm.data.model
-
-/**
- * Created by wonyoung on 2017. 10. 27..
- */
-data class DummyData(val name:String)
--- app/src/main/java/my/toru/aacmvvm/data/remote/RemoteRepository.kt
+++ app/src/main/java/my/toru/aacmvvm/data/remote/RemoteRepository.kt
@@ -33,50 +33,5 @@ class RemoteRepository {
                     .addConverterFactory(GsonConverterFactory.create())
                     .build()
         }
-
-        fun testDataUpdate(): MutableLiveData<List<String>> {
-            Log.w("TORU","testDataUpdate Load Start")
-
-            val dataList = ArrayList<String>()
-            dataList.add("1")
-            dataList.add("2")
-            dataList.add("3")
-            dataList.add("4")
-            dataList.add("5")
-            dataList.add("6")
-            dataList.add("7")
-            dataList.add("8")
-            dataList.add("9")
-            dataList.add("10")
-
-            Log.w("TORU", "datalist size: " + dataList.size)
-
-            val returnvalue = MutableLiveData<List<String>>()
-            returnvalue.value = dataList
-            return returnvalue
-        }
-
-        fun testDataUpdate2(liveData:MutableLiveData<ArrayList<String>>){
-            Log.w("TORU","testDataUpdate2 Load Start")
-
-            val dataList = ArrayList<String>()
-            dataList.add("11")
-            dataList.add("21")
-            dataList.add("31")
-            dataList.add("41")
-            dataList.add("51")
-            dataList.add("61")
-            dataList.add("71")
-            dataList.add("81")
-            dataList.add("91")
-            dataList.add("101")
-            dataList.add("181")
-            dataList.add("191")
-            dataList.add("201")
-
-            liveData.value = dataList
-
-            Log.w("TORU", "datalist size: " + dataList.size)
-        }
     }
 }
