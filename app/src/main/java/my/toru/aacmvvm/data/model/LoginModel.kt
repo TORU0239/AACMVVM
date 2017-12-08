3721faf685a7b3c940c1d7ba55d1f6c5c862312d
Toru Choi <toru.choi@11street.my>
2017-12-08 17:40:28 +0800

    databinding

--- app/src/main/java/my/toru/aacmvvm/data/model/LoginModel.kt
+++ app/src/main/java/my/toru/aacmvvm/data/model/LoginModel.kt
@@ -2,6 +2,9 @@ package my.toru.aacmvvm.data.model
 
 import android.databinding.BaseObservable
 import android.databinding.Bindable
+import android.text.Editable
+import android.text.TextWatcher
+import android.util.Log
 import my.toru.aacmvvm.BR
 
 /**
@@ -17,6 +20,7 @@ class LoginModel : BaseObservable() {
     @Bindable get
     set(value){
         field = value
+        Log.w(TAG, "email::$field")
         notifyPropertyChanged(BR.emailAddress)
     }
 
@@ -24,7 +28,35 @@ class LoginModel : BaseObservable() {
     @Bindable get
     set(pwd) {
         field = pwd
+        Log.w(TAG, "password::$field")
         notifyPropertyChanged(BR.passWord)
     }
 
+    @Bindable
+    fun getEmailTextWatcher():TextWatcher{
+        return object:TextWatcher{
+            override fun afterTextChanged(s: Editable?) {
+                emailAddress = s.toString()
+                notifyChange()
+            }
+
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {}
+
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {}
+        }
+    }
+
+    @Bindable
+    fun getPasswordTextWatcher():TextWatcher{
+        return object:TextWatcher{
+            override fun afterTextChanged(s: Editable?) {
+                passWord = s.toString()
+                notifyChange()
+            }
+
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {}
+
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {}
+        }
+    }
 }
--- app/src/main/java/my/toru/aacmvvm/ui/MainActivity.kt
+++ app/src/main/java/my/toru/aacmvvm/ui/MainActivity.kt
@@ -1,10 +1,13 @@
 package my.toru.aacmvvm.ui
 
+import android.arch.lifecycle.ViewModelProviders
 import android.databinding.DataBindingUtil
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import my.toru.aacmvvm.R
+import my.toru.aacmvvm.data.model.LoginModel
 import my.toru.aacmvvm.databinding.MainActivityBinding
+import my.toru.aacmvvm.viewmodel.LoginViewModel
 
 /**
  * Created by toruchoi on 07/12/2017.
@@ -14,5 +17,13 @@ class MainActivity: AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         val dataBinding : MainActivityBinding = DataBindingUtil.setContentView(MainActivity@this, R.layout.main_activity)
+        val loginVM = ViewModelProviders.of(MainActivity@this).get(LoginViewModel::class.java)
+
+        dataBinding.loginModel = LoginModel()
+        dataBinding.executePendingBindings()
+
+        dataBinding.btnLogin.setOnClickListener{
+            loginVM.onLoginButtonClicked()
+        }
     }
 }
