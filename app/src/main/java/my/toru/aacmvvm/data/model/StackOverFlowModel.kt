ffade3f360dca12d6cd0a5f032c7d60ca2d9d265
Toru Choi <toru.choi@11street.my>
2017-12-12 19:01:27 +0800

    question owner

--- app/src/main/java/my/toru/aacmvvm/data/model/StackOverFlowModel.kt
+++ app/src/main/java/my/toru/aacmvvm/data/model/StackOverFlowModel.kt
@@ -28,8 +28,47 @@ data class StackOverFlowModel(val items: ArrayList<StackOverFlowItemModel>) : Pa
     }
 }
 
+data class OwnerItemModel(val reputation: String,
+                          @SerializedName("user_id") val userId: String,
+                          @SerializedName("user_type") val userType: String,
+                          @SerializedName("accept_rate") val acceptRate: String,
+                          @SerializedName("profile_image") val profileImage: String,
+                          @SerializedName("display_name") val displayName: String,
+                          val link: String) : Parcelable {
+    constructor(source: Parcel) : this(
+            source.readString(),
+            source.readString(),
+            source.readString(),
+            source.readString(),
+            source.readString(),
+            source.readString(),
+            source.readString()
+    )
+
+    override fun describeContents() = 0
+
+    override fun writeToParcel(dest: Parcel, flags: Int) = with(dest) {
+        writeString(reputation)
+        writeString(userId)
+        writeString(userType)
+        writeString(acceptRate)
+        writeString(profileImage)
+        writeString(displayName)
+        writeString(link)
+    }
+
+    companion object {
+        @JvmField
+        val CREATOR: Parcelable.Creator<OwnerItemModel> = object : Parcelable.Creator<OwnerItemModel> {
+            override fun createFromParcel(source: Parcel): OwnerItemModel = OwnerItemModel(source)
+            override fun newArray(size: Int): Array<OwnerItemModel?> = arrayOfNulls(size)
+        }
+    }
+}
+
 data class StackOverFlowItemModel(@SerializedName("is_answered") val isAnswered: Boolean,
                                   @SerializedName("view_count") val viewCount: String,
+                                  val owner: OwnerItemModel,
                                   val score: String,
                                   val link: String,
                                   val title: String,
@@ -37,6 +76,7 @@ data class StackOverFlowItemModel(@SerializedName("is_answered") val isAnswered:
     constructor(source: Parcel) : this(
             1 == source.readInt(),
             source.readString(),
+            source.readParcelable<OwnerItemModel>(OwnerItemModel::class.java.classLoader),
             source.readString(),
             source.readString(),
             source.readString(),
@@ -48,6 +88,7 @@ data class StackOverFlowItemModel(@SerializedName("is_answered") val isAnswered:
     override fun writeToParcel(dest: Parcel, flags: Int) = with(dest) {
         writeInt((if (isAnswered) 1 else 0))
         writeString(viewCount)
+        writeParcelable(owner, 0)
         writeString(score)
         writeString(link)
         writeString(title)
